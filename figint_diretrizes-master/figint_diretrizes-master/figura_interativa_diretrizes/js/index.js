fcde7fa08752f4000791d330c1bdef68df4183e4
Allyne Beatriz Nascimento <48529128+allyneb@users.noreply.github.com>
2020-10-22 15:56:00 -0300

    Add files via upload

---
+++ figint_diretrizes-master/figint_diretrizes-master/figura_interativa_diretrizes/js/index.js
@@ -0,0 +1,72 @@
+   // a cross reference of area names to text to be shown for each area
+   var xref = {
+    carrots: "<b>Lugar intermediário de apoio:</b> O Apoio não é um lugar neutro. É necessário ficar claro, que independente de qual seja o lugar institucional da pessoa que desenvolve a Função Apoio, ele jamais ocupará um lugar de neutralidade. Neste “lugar intermediário do apoiador, nem gestor, nem profissional da equipe”, é importante esclarecer os objetivos e expectativas dos atores inseridos nesse processo e delimitar a certa autonomia para o trabalho do apoio para mitigar o desconforto dessas situações.",
+    asparagus: "<b>Dimensão Pedagógica:</b> O apoiador deve desenvolver sua atuação para além do caminho tradicional de ensino aprendizagem, centrado apenas em aspectos clínicos e epidemiológicos, pois essa postura valoriza apenas o saber científico, produzido em manuais, normas e consensos e não leva em conta outras formas de produção de saberes existentes na área da saúde. A dimensão pedagógica da atuação do Apoiador, junto às equipes apoiadas, merece destacar: a) a primeira é sempre incluir o “como fazer e com quem fazer”, depois da abordagem teórica; b) a segunda écompreender, estimular e apoiar os profissionais para analisarem a organização do processo de trabalho, os dilemas cotidianos dos serviços; como as equipes se relacionam e como as posturas dos gestores tem afetado positiva ou negativamente as mesmas e ajudar ao grupo a refletir sobre suas práticas, buscando aumentar a capacidade de análise e de intervenção na realidade.",
+    squash: "<b>Análise de contexto:</b> Para se instituir a função Apoio, deve haver um grau mínimo de investimento e de vontade institucional para alguma abertura à mudança. Todavia, não existe um momento certo e nem as condições ideais para instituição do apoio nas equipes. Assim, é necessário realizar uma análise das dificuldades institucionais, para que possam ser debatidas, como um fator de limite, mas não definidor da instituição do apoio às equipes de saúde. É possível observar no cotidiano do trabalho em saúde, estruturas organizativas que mantêm as lógicas corporativas, hierárquicas, burocratas e centradas em procedimentos, e ao mesmo tempo, a coexistência de diretrizes democráticas, contribuindo para a fragmentação do cuidado e dificultando a integração entre os profissionais. Nesse caso, o apoiador deveria identificar parceiros(as), dentro das equipes dos profissionais e buscar contribuir para qualificar a rede assistencial e investir nas estratégias da cogestão.",
+    redpepper: "<b>Vínculo:</b> É algo que liga as pessoas, e indica interdependência, relações de compromissos entre profissionais com os pacientes e vice e versa. A constituição de vinculo do apoiador na prática depende do movimento do mesmo e da equipe. Do lado da equipe, só se efetivará o vínculo, quando ela acreditar que o Apoiador poderá contribuir, de algum modo, para qualificar o processo de trabalho. (Campos, 2003, p.53). O vínculo é uma diretriz central na prática do apoio, na qual o apoiador pode utilizar estratégias para facilitar a sua produção: a)definir por quantas equipes cada apoiador irá se responsabilizar; ouvir as demandas e problemas da equipe e não levar soluções prontas; b) combinar conjuntamente as formas de acionar o Apoio e com quais finalidades e ter claro que momentos serão abertos ao apoiador e c)compreender a instituição onde se insere a equipe apoiada, seus profissionais. ",
+    yellowpepper: "<b>Demandas e Oferecimentos:</b> O apoiador deve ter a capacidade de trabalhar com análises de demandas e de oferecimento junto a equipe apoiada.” (Castro, Dantas, p. 115). A função do apoiador no processo de análise, além de facilitar o autoconhecimento dos desejos, resistências e interesses dos sujeitos, é possibilitar a coprodução de análises, conhecimentos e necessidades sociais. Dessa forma, o apoiador visa facilitar, estimular e contribuir com a coprodução e a análise de demandas e ofertas, buscando aumentar a capacidade analítica e reflexiva e a capacidade de ação dos sujeitos e coletivos, contribuindo para a reformulação da sua práxis.",
+    celery: "<b>Colocar-se na roda:</b> A participação do apoiador nos “espaços coletivos” é fundamental e muito importante no desenvolvimento de suas atividades, para buscar alcançar os seus objetivos, a partir da compreensão dialógica e de compartilhamento, que só será possível, se houver escuta, reflexão, construção de definições de temas relevantes e divisão de tarefas entre os sujeitos envolvidos.",
+    cucumbers: "<b>Construção de autonomia:</b> A função do Apoio é essencial buscar a autonomia da equipe apoiada, o que não significa que aquela autonomia, quer dizer descompromisso, pelo contrário, a ampliação da mesma depende da capacidade do sujeito reconhecer e lidar com a rede de dependência na qual estamos todos envolvidos (Campos, 2010). O apoiador precisa necessariamente situar-se como num fio condutor e não como o “dono da última palavra”, buscando contribuir para reflexão sobre como desenvolver maior autonomia para os profissionais e os usuários, estimular movimentos que ajude ao grupo a perseguir o caminho de suas próprias mudanças, o que requer estar aberto a perguntas e as novas possibilidades. O apoio funciona quando a equipe amplia seus referenciais de modo crítico, aderindo ao mesmo de forma voluntária e construindo significados para as atividades realizadas em parceria junto com o apoiador.",
+    broccoli: "<b>Construção de Objetos de investimento:</b> O apoiador colabora para a equipe de apoiados construir seus objetos de investimentos. Não deve se colocar apenas numa posição de escuta e também não deve impor projetos e ações à revelia dos interesses do grupo. É recomendável, que o apoiador busque criar empatia com a equipe apoiada, e desde o início procure desmistificar o seu “suposto poder” de agir em seu nome e realizar em seu lugar as tarefas que competem a mesma, para conseguir os objetivos que deseja alcançar. As possibilidades de relação e de comunicação do apoiador com o grupo, não podem e nem devem se tornarem burocráticas, prescritivas e normativa, em que o “fazer não se acompanha da vontade de “querer fazer”, uma vez que não haverá implicação dos sujeitos e nem vontade de produzir ações a partir do protagonismo e participação da equipe. O apoiador deve lembrar que o lugar do “apoio” não é um lugar neutro, e que existem variações no “modo de fazer” apoio, conforme o lugar institucional, o conhecimento de métodos e teorias e a capacidade de lidar com conflitos e afetos” (Castro; Dantas, p. 118. 1917).",
+    dip: "<b>Núcleos de análise:</b> A construção de núcleos de análises é um ponto importante e de destaque na Função Apoio. Estes núcleos podem ser considerados como objeto de reflexão e análise de uma equipe, para que signifiquem aspectos da realidade (fazer síntese e desenvolvam ações práticas de intervenção sobre essa realidade). Assim sendo, a fala de um membro da equipe ou algum acontecimento que se destaque na rotina de trabalho, pode se caracterizar como um núcleo análise, bem como, programas, normas e indicadores também podem se constituir núcleos de análise"
+};
+
+var defaultDipTooltip = '';
+
+var image = $('#diretrizes');
+
+image.mapster(
+{
+    fillOpacity: 0.4,
+    fillColor: "d42e16",
+    stroke: true,
+    strokeColor: "3320FF",
+    strokeOpacity: 0.8,
+    strokeWidth: 4,
+    singleSelect: true,
+    mapKey: 'name',
+    listKey: 'name',
+    onClick: function (e) {
+        var newToolTip = defaultDipTooltip;
+        
+        // update text depending on area selected
+        $('#selections').html(xref[e.key]);
+        
+        // if Asparagus selected, change the tooltip
+        if (e.key === 'asparagus') {
+            newToolTip = "OK. I know I have come down on the dip before, but let's be real. "
+                +"Raw asparagus without any of that delicious ranch and onion dressing "
+                +"slathered all over it is not so good.";
+        }
+        image.mapster('set_options', { 
+            areas: [{
+                key: "dip",
+                toolTip: newToolTip
+                }]
+            });
+    },
+    showToolTip: true,
+    toolTipClose: ["tooltip-click", "area-click"],
+    areas: [
+        {
+            key: "redpepper",
+            fillColor: "ffffff"
+        },
+        {
+            key: "yellowpepper",
+            fillColor: "000000"
+        },
+        {
+            key: "carrots",
+            fillColor: "000000"
+        },
+        {
+            key: "dip",
+            toolTip: defaultDipTooltip
+        },
+        {
+            key: "asparagus",
+            strokeColor: "FFFFFF"
+        }
+        ]
+});
